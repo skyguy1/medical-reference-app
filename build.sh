37f397f59af4a95f4617c789974b4a129d6f0dff
skyguy1 <skylergreenwald@gmail.com>
2025-03-06 16:34:34 -0800

    Create minimal deployment configuration

--- build.sh
+++ build.sh
@@ -2,8 +2,4 @@
 # exit on error
 set -o errexit
 
-pip install --upgrade pip
 pip install -r requirements.txt
-
-# Create database tables
-python -c "import os; from app import app, db; app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL'); app.app_context().push(); db.create_all()"
